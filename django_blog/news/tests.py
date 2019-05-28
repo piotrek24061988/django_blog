4ca9620099f262eaf3ffa167ed7c73b2dc3733f5
Gorecki Piotr <piotr.gorecki@meelogic.com>
2019-05-28 22:44:29 +0200

    first automatic testing added

--- django_blog/news/tests.py
+++ django_blog/news/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import unittest
+from . import views
 
-# Create your tests here.
+class HomeTestCase(unittest.TestCase):
+    def test_home(self):
+        # Setup.
+        request = 'fake request'
+        response_status = 200
+        response_content = b'<h1>News Home</h1>'
+        # Run.
+        response = views.home(request)
+        # Check.
+        self.assertEqual(response.status_code, response_status)
+        self.assertEqual(response.content, response_content)
+
+    def test_about(self):
+        # Setup.
+        request = 'fake request'
+        response_status = 200
+        response_content = b'<h1>News About</h1>'
+        # Run.
+        response = views.about(request)
+        # Check.
+        self.assertEqual(response.status_code, response_status)
+        self.assertEqual(response.content, response_content)
--- django_blog/news/views.py
+++ django_blog/news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>News Home</h1>');
+    return HttpResponse('<h1>News Home</h1>')
 
 def about(request):
-    return HttpResponse('<h1>News About</h1>');
+    return HttpResponse('<h1>News About</h1>')
