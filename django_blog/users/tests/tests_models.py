4c6eb9cca5de383f1af2ee0c13988dab0b548a24
Gorecki Piotr <piotr.gorecki@meelogic.com>
2019-06-06 08:34:49 +0200

    users models test fix

--- django_blog/users/tests/tests_models.py
+++ django_blog/users/tests/tests_models.py
@@ -9,14 +9,18 @@ class UsersModelsTestCases(unittest.TestCase):
         name = 'UsersTestUser'
         mail = name + '@mail.com'
         password = '1234'
+        image_url = 'profile_pics/default.jpg'
+        image_url_prefix = '/media/'
         # Run
         test_user = User.objects.create_user(name, mail, password)
         response = User.objects.filter(username=name).first()
         # Check
         self.assertEqual(response, test_user)
         # Run
-        #test_profile = models.Profile(user=test_user, image=None)
+        # Lines below are done automatically by signals.py and this is why
+        # are commented out here.
+        #test_profile = models.Profile(user=test_user, image=image_url)
         #test_profile.save()
         # Check
         #self.assertEqual(test_profile, test_user.profile)
-        #self.assertEqual(None, test_user.profile.image)
+        self.assertEqual(image_url_prefix + image_url, test_user.profile.image.url)
