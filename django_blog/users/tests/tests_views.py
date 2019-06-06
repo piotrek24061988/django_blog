873be2a9cbe39f3ce5ba7ce159f83e233b034b33
Gorecki Piotr <piotr.gorecki@meelogic.com>
2019-06-06 08:54:56 +0200

    users views test improvement

--- django_blog/users/tests/tests_views.py
+++ django_blog/users/tests/tests_views.py
@@ -9,9 +9,8 @@ class RequestStub:
 
     META = {'CSRF_COOKIE': []}
     user = USER
-
-    def method(self):
-        pass
+    method = 'POST'
+    POST = {'POST': {}}
 
 
 class UsersViewsTestCases(unittest.TestCase):
@@ -26,13 +25,24 @@ class UsersViewsTestCases(unittest.TestCase):
         self.assertEqual(response.status_code, response_status)
         self.assertIn(response_content, response.content)
 
-    def test_profile_authenticated(self):
+    def test_profile_post(self):
         # Setup
         request = RequestStub
         response_status = 200
         response_content = b'Profile Title'
         # Run
-        request.USER.authenticated = True
+        response = views.profile(request)
+        # Check
+        self.assertEqual(response.status_code, response_status)
+        self.assertIn(response_content, response.content)
+
+    def test_profile_get(self):
+        # Setup
+        request = RequestStub
+        response_status = 200
+        response_content = b'Profile Title'
+        # Run
+        request.method = 'GET'
         response = views.profile(request)
         # Check
         self.assertEqual(response.status_code, response_status)
