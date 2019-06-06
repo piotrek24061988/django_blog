fd3a378ad90c23ac3ee9467ff3cb80fe40aa2153
Gorecki Piotr <piotr.gorecki@meelogic.com>
2019-06-06 08:44:28 +0200

    profile str test

--- django_blog/users/tests/tests_models.py
+++ django_blog/users/tests/tests_models.py
@@ -24,3 +24,20 @@ class UsersModelsTestCases(unittest.TestCase):
         # Check
         #self.assertEqual(test_profile, test_user.profile)
         self.assertEqual(image_url_prefix + image_url, test_user.profile.image.url)
+
+    def test_str(self):
+        # Setup
+        name = 'UsersTestUser2'
+        mail = name + '@mail.com'
+        password = '1234'
+        image_url = 'profile_pics/default.jpg'
+        profile_str = 'Profile'
+        # Run
+        test_user = User.objects.create_user(name, mail, password)
+        response = User.objects.filter(username=name).first()
+        # Check
+        self.assertEqual(response, test_user)
+        # Run
+        test_profile = models.Profile(user=test_user, image=image_url)
+        # Check
+        self.assertEqual(test_profile.__str__(), name + profile_str)
