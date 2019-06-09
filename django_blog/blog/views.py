7dbc7835f13cd91384e8098ed7b0e8afcb8c533f
Gorecki Piotr <piotr.gorecki@meelogic.com>
2019-06-09 16:14:24 +0200

    adding/deleting/updateing blog posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -23,6 +24,39 @@ class PostDetailView(DetailView):
     template_name = 'blog_post.html'
 
 
-class PostCreateView(DetailView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    template_name = 'blog_post.html'
+    fields = ['title', 'content']
+    template_name = 'blog_update.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog_update.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog_delete.html'
+    success_url = '/blog'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
