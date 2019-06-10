9930f5c3e581f6d950c71261846a77c0d8603a25
Gorecki Piotr <piotr.gorecki@meelogic.com>
2019-06-10 13:42:08 +0200

    tests for blog with content from json added

--- django_blog/blog/tests/tests_models.py
+++ django_blog/blog/tests/tests_models.py
@@ -1,7 +1,6 @@
-import unittest
+import unittest, json
 from .. import models
 from django.contrib.auth.models import User
-
 
 class BlogModelsTestCases(unittest.TestCase):
     def test_user_empty(self):
@@ -46,3 +45,18 @@ class BlogModelsTestCases(unittest.TestCase):
         # Check
         self.assertEqual(topic, models.Post.objects.last().title)
         self.assertEqual(message, models.Post.objects.last().content)
+
+    def test_json_post(self):
+        # Setup
+        with open('blog/tests/posts.json') as f:
+            posts_json = json.load(f)
+        # At least 2 users needed
+        user1 = User.objects.create_user('PostUser3', 'PostUser3@gmail.com', '1234')
+        user2 = User.objects.create_user('PostUser4', 'PostUser4@gmail.com', '1234')
+        for post_json in posts_json:
+            # Run
+            post = models.Post(title=post_json['title'], content=post_json['content'], author_id=post_json['user_id'])
+            post.save()
+            # Check
+            self.assertEqual(post_json['title'], models.Post.objects.last().title)
+            self.assertEqual(post_json['content'], models.Post.objects.last().content)
